8405bfc734c89c965ead002ffbc5d6997a30d78c
다은김 <daeun-kim@da-eungim-ui-noteubug.local>
2026-02-11 00:14:54 +0900

    Finish responsive layout

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const toggleThemeBtn = document.querySelector('.header__theme-button');
+// const toggleThemeBtn = document.querySelector('.header__theme-button');
 
-document.onload = setInitialTheme(localStorage.getItem('theme'));
-function setInitialTheme(themeKey) {
-    if (themeKey === 'dark') {
-        document.documentElement.classList.add('darkTheme');
-    } else {
-        document.documentElement.classList.remove('darkTheme');
-    }
-}
+// document.onload = setInitialTheme(localStorage.getItem('theme'));
+// function setInitialTheme(themeKey) {
+//     if (themeKey === 'dark') {
+//         document.documentElement.classList.add('darkTheme');
+//     } else {
+//         document.documentElement.classList.remove('darkTheme');
+//     }
+// }
 
-toggleThemeBtn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('darkTheme');
+// toggleThemeBtn.addEventListener('click', () => {
+//     document.documentElement.classList.toggle('darkTheme');
 
-    if (document.documentElement.classList.contains('darkTheme')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-})
+//     if (document.documentElement.classList.contains('darkTheme')) {
+//         localStorage.setItem('theme', 'dark');
+//     } else {
+//         localStorage.setItem('theme', 'light');
+//     }
+// });
+
+
+const btn = document.querySelector(".more-btn");
+const caption = btn.previousElementSibling;
+btn.addEventListener("click", () => caption.classList.toggle("caption-text-open"));
+
+
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const storyBar = document.querySelector('.story-bar');
+
+nextBtn.onclick = () => {
+    storyBar.scrollBy({ left: 300, behavior: 'smooth' });
+};
+
+prevBtn.onclick = () => {
+    storyBar.scrollBy({ left: -300, behavior: 'smooth' });
+};
+
+storyBar.onscroll = () => {
+    prevBtn.style.display = storyBar.scrollLeft > 0 ? 'flex' : 'none';
+    
+    const isEnd = storyBar.scrollLeft + storyBar.clientWidth >= storyBar.scrollWidth - 10;
+    nextBtn.style.display = isEnd ? 'none' : 'flex';
+};
